fd0d68cfec789639fb3672d191dffda90c9e8513
zupalex <alex_someone@hotmail.com>
2015-09-02 02:30:08 +0200

    A lot of additions...
    Changelog:
    *Machine component is set. Turning On the machine starts the digging
    process, turning it off stops it (functionnal)
    *Digging function : Fully functionnal. It digs stuffs according the the
    biome it is on and reads the file scripts/mmloottable.lua to get the
    probability of each items. It then store the result in the attached
    chest.
    *Break Down mechanism : Implemented and functionnal. When failing to
    dig, there is a currently 15% chance to break down the machine. If the
    storage is full but the machine keeps on digging there is a 10% chance
    that it breaks down (it damages the mechanism by trying to overfill the
    chest). The item is lost in the process.
    *I added a new component (mnzmachines.lua) to handle the new action
    "MMREPAIR", to repair the machine when it breaks down. currently there
    is no wrench implemented so i just use a gears. The MMREPAIR action is
    functionnal and implemented.
    *There is a chance to "dig" a mob. I added to mmloottable.lua 2 new
    etries. I will explain the first one here. CHESTMOBS are the mobs that
    can be "dug" and it changes with the biome you are currently on. When a
    mob is "dug", it is stored temporarily in a mnzmachines variable
    (mnzmachines.surpriseinchest) which is a table which can contain up to 3
    elements (you cannot overcrowd your chest...) each with 3 entities, for
    a max of 9 mobs to spawn at the same time when the player open the
    storage.
    *Added the possibility of larger creatures to spawn at the surface next
    to the machine. This is also biome dependant and it is handled by the
    second new entry in the mmloottable.lua called "ESCAPEMOBS". I tested
    it, it's functionnal. To prevent spamming during playtesting I put the
    function to be run 4 times per day (to be discussed for the final
    value).
    *The chance for a mob to be "dug" or to escape are configurable.
    *Player cannot put him/herself item in the machine storage. It is just
    possible to retrieve items from here.
    
    -> For the final design : For the whole stuff to make sense, the hole
    under the machine will have to be big enough so rock lobsters or
    bunnyman could pass through it (at least not be completly tiny)

---
+++ scripts/mmloottable.lua
@@ -0,0 +1,328 @@
+MMLOOTTABLE = {}
+
+MMLOOTTABLE["BIOME_"..GROUND.ROCKY] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.7,
+		rocks = 0.33,
+		flint = 0.67,
+		nitre = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.91,
+		ironore = 0.38,
+		goldnugget = 0.67,
+		redgem = 0.84,
+		bluegem = 1,
+	},
+	
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		marble = 0.67,
+		moonrocknugget = 1,
+	},
+	
+	CHESTMOBS =
+	{
+		bat = 0.33,
+		slurtle = 0.67,
+		snurtle = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		rocky = 1,
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.SAVANNA] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.6,
+		rocks = 0.23,
+		flint = 0.56,
+		nitre = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.81,
+		goldnugget = 0.33,
+		ironore = 0.81,
+		horn = 1,
+	},
+	
+	TIERTHREE =
+	{	
+		tierthreshold = 1,
+		manrabbit_tail = 0.26,
+		slurper_pelt = 0.42,
+		orangegem = 0.58,
+		trinket_2 = 0.69,
+		trinket_3 = 0.80,
+		trinket_11 = 0.91,
+		trinket_9 = 1,
+	},
+		
+	CHESTMOBS =
+	{
+		bat = 0.5,
+		monkey = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		bunnyman = 1,
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.GRASS] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.7,
+		rocks = 0.33,
+		flint = 0.76,
+		nitre = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.89,
+		ironore = 0.32,
+		goldnugget = 0.58,
+		boneshard = 1,
+	},
+	
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		trinket_1 = 0.18,
+		trinket_4 = 0.36,
+		trinket_5 = 0.54,
+		trinket_13 = 0.73,
+		yellowgem = 1,
+	},
+	
+	CHESTMOBS =
+	{
+		bat = 0.5,
+		mole = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		bunnyman = 0.66,
+		rocky = 1,
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.FOREST] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.6,
+		rocks = 0.25,
+		flint = 0.58,
+		nitre = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.9,
+		lightbulb = 0.17,
+		goldnugget = 0.50,
+		ironore = 0.83,
+		bluegem = 1,
+	},
+	
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		mandrake = 0.5,
+		greengem = 0.8,
+		slurtlehat = 1,
+	},
+	
+	CHESTMOBS =
+	{
+		bat = 0.33,
+		spider_dropper = 0.67,
+		monkey = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		bunnyman = 0.4,
+		spider_hider = 0.73,
+		spider_spitter = 1,
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.MARSH] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.7,
+		wetgoop = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.85,
+		purplegem = 0.33,
+		cutlichen = 0.33,
+		tentaclespots = 1,
+	},
+	
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		trinket_12 = 0.33,
+		trinket_6 = 0.33,
+		tentaclespike = 1,
+	},
+		
+	CHESTMOBS =
+	{
+		monkey = 0.33,
+		spider_dropper = 0.67,
+		snurtle = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		spider_hider = 0.33,
+		spider_spitter = 0.67,
+		spider_dropper = 1,
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.CARPET] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.4,
+	},
+
+	TIERTWO =
+	{
+		tierthreshold = 0.94,
+		marble = 0.56,
+		gears = 0.75,
+		purplegem = 0.94,
+		redgem = 0.97,
+		bluegem = 1,
+	},
+	
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		greengem = 0.33,
+		yellowgem = 0.67,
+		orangegem = 1,
+	},
+	
+	CHESTMOBS =
+	{
+		slurper = 0.75,
+		bat = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		knight_nightmare = 0.5,
+		bishop_nightmare = 1,		
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.DECIDUOUS] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.6,
+		rocks = 0.25,
+		flint = 0.50,
+		nitre = 0.75,
+		feather_crow = 0.83,
+		feather_robin = 0.91,
+		feather_robin_winter = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.85,
+		ironore = 0.4,
+		goldnugget = 0.8,
+		coontail = 1, 
+	},
+	
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		transistor = 0.33,
+		glommerfuel = 0.67,
+		greengem = 1,
+	},
+	
+	CHESTMOBS =
+	{
+		bat = 0.4,
+		monkey = 0.7,
+		mole = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		bunnyman = 0.5,
+		spider_hider = 0.5,
+	}
+}
+
+MMLOOTTABLE["BIOME_"..GROUND.DESERT_DIRT] = 
+{
+	TIERONE =
+	{
+		tierthreshold = 0.38,
+		rocks = 0.61,
+		flint = 1,
+	},
+	
+	TIERTWO =
+	{
+		tierthreshold = 0.87,
+		boneshards  = 0.41,
+		ironore = 0.61,
+		goldnugget = 0.75,
+		redgem = 0.85,
+		houndstooth = 1,
+	},
+		
+	TIERTHREE =
+	{
+		tierthreshold = 1,
+		trinket_8 = 0.15,
+		trinket_7 = 0.30,
+		sunken_boat_trinket_2 = 0.45,
+		sunken_boat_trinket_5 = 0.60,
+		lightninggoathorn = 0.83,
+		dragon_scales = 1,
+	},
+	
+	CHESTMOBS =
+	{
+		slurper = 0.33,
+		bat = 1,
+	},
+	
+	ESCAPEMOBS = 
+	{
+		rocky = 0.5,
+		spider_spitter = 0.5,
+	}
+}
--- scripts/prefabs/miningmachine.lua
+++ scripts/prefabs/miningmachine.lua
@@ -10,6 +10,162 @@ prefabs = {
 
 -----------------------------------------------------------THE MACHINE ITSELF-----------------------------------------------------
 
+local function FillStorageWithSurpriseMob(inst, mob)
+	local mms = inst.mmstorage
+	local chestmobtable = mms.components.mnzmachines.surpriseinchest
+
+	if chestmobtable[mob] == nil then
+		chestmobtable[mob] = 1
+	elseif chestmobtable[mob] < 3 then
+		chestmobtable[mob] = chestmobtable[mob] + 1
+	end
+end
+
+local function StopDigging(inst)
+	if inst.diggingtask ~= nil then
+		inst.diggingtask:Cancel()
+		inst.diggingtask = nil
+	end
+end
+
+local function DiggingJob(inst)
+	print("*********** I start the digging action ***********")
+	
+	if math.random() < 0.7 then -- make the success odd configurable later
+		print("I digged successfully! :-)")
+		
+		local tiletype = TheWorld.Map:GetTileAtPoint(inst.Transform:GetWorldPosition())
+		print("Tile Type is ", tiletype)
+
+		local tier_draw = math.random()
+		local tier_loot
+		
+		if tier_draw <= MMLOOTTABLE["BIOME_"..tiletype].TIERONE.tierthreshold then
+			tier_loot = "TIERONE"
+		elseif tier_draw > MMLOOTTABLE["BIOME_"..tiletype].TIERONE.tierthreshold and tier_draw <= MMLOOTTABLE["BIOME_"..tiletype].TIERTWO.tierthreshold then
+			tier_loot = "TIERTWO"	
+		else
+			tier_loot = "TIERTHREE"		
+		end
+		
+		print("Random Tier Drawing = ", tier_draw, " <-> ", tier_loot)
+		
+		for n, m in pairs(MMLOOTTABLE["BIOME_"..tiletype][tier_loot]) do
+			if n ~= "tierthreshold" then
+				local current_minodd = 0
+				for k, v in pairs(MMLOOTTABLE["BIOME_"..tiletype][tier_loot]) do
+					if k ~= "tierthreshold" and v > current_minodd and v < m then
+						current_minodd = v
+					end
+				end
+				
+				print("Probability to dig ", n, " = ", (m - current_minodd)  * 100, "%")
+			end
+		end
+		
+		local item_draw = math.random()
+		local closest_th = 2
+		local item_to_dig = nil
+		for k, v in pairs(MMLOOTTABLE["BIOME_"..tiletype][tier_loot]) do
+			if k ~= "tierthreshold" and v < closest_th and v > item_draw then
+				closest_th = v
+				item_to_dig = k
+			end
+		end
+		
+		print("---> Item to Dig  ", item_to_dig or "ERROR", " (draw = ", item_draw, ")")
+		
+		local mms = inst.mmstorage
+		
+		if item_to_dig then
+			print("------> Storage = ", inst.mmstorage, "(found the container component : ", inst.mmstorage.components.container, ")")
+			local item_to_store = SpawnPrefab(item_to_dig)
+			local storeincontainer = mms.components.container:GiveItem(item_to_store, nil, nil, false)
+			
+			if not storeincontainer then
+				if math.random() < 0.10 then
+					print("Container full, damaging pipe : Machine Jammed!!")
+					inst.components.machine:TurnOff()
+					inst:DoTaskInTime(0.6, function () -- to prevent a DoTaskInTime of the TunOff function to interfere after 0.5 seconds, I execute it after 0.6 seconds
+												inst.components.mnzmachines.jammed = true 
+											end
+									)
+				end
+			end
+		end
+		
+		local chestmob_draw = math.random()
+		if mms.components.mnzmachines.surpriseinchest and #mms.components.mnzmachines.surpriseinchest < 3 and chestmob_draw < TUNING.CHESTMOBSODDS then
+			closest_th = 2
+			local mob_to_store = nil
+			for k, v in pairs(MMLOOTTABLE["BIOME_"..tiletype]["CHESTMOBS"]) do
+				if v < closest_th and v > chestmob_draw/TUNING.CHESTMOBSODDS then
+					closest_th = v
+					mob_to_store = k
+				end
+			end
+			print("I brought a surprise with the loot : ", mob_to_store)
+			FillStorageWithSurpriseMob(inst, mob_to_store)
+		end
+	else
+		if math.random() < 0.15 then
+			print("Failed Digging Task : Machine Jammed!!")
+			inst.components.machine:TurnOff()
+			inst:DoTaskInTime(0.6, function () -- to prevent a DoTaskInTime of the TunOff function to interfere after 0.5 seconds, I execute it after 0.6 seconds
+										inst.components.mnzmachines.jammed = true 
+									end
+							)
+		end
+	end
+	
+	print("I performed my digging action!")
+end
+
+local function InitializeDigging(inst)
+	if inst.diggingtask == nil then
+		inst.diggingtask = inst:DoPeriodicTask(5, DiggingJob) -- adjust the periodicity later with TUNING.SEG_TIME (make it configurable anyway)
+	end
+end
+
+local function TurnOff(inst, instant)
+	inst.on = false
+
+	StopDigging(inst)
+	
+	-- inst.sg:GoToState("turn_off")
+end
+
+local function TurnOn(inst, instant)
+	inst.on = true
+	
+	if not inst:HasTag("jammed") then
+		InitializeDigging(inst)
+	end
+	
+	-- inst.sg:GoToState("turn_on")
+end
+
+local function CheckForEscapeMobs(inst)	
+	print("========= Check for a Mob Escape ============")
+	local escmob_draw = math.random()
+	
+	if escmob_draw < TUNING.ESCMOBSODDS then
+		print("A Mob Escaped!")
+		local tiletype = TheWorld.Map:GetTileAtPoint(inst.Transform:GetWorldPosition())
+		local closest_th = 2
+		local mob_to_esc
+		for k, v in pairs(MMLOOTTABLE["BIOME_"..tiletype]["ESCAPEMOBS"]) do
+			if v < closest_th and v > escmob_draw/TUNING.ESCMOBSODDS then
+				closest_th = v
+				mob_to_esc = k
+			end
+		end
+		print("A Mob Escaped! : ", mob_to_esc)
+		local escmob = SpawnPrefab(mob_to_esc)
+		escmob.Transform:SetPosition(inst.Transform:GetWorldPosition())
+	end
+end
+
 local function miningmachinefn()
 	local inst = CreateEntity()
 
@@ -36,10 +192,10 @@ local function miningmachinefn()
 
 	inst:AddComponent("inspectable")
 	
-	-- inst:AddComponent("machine")
-	-- inst.components.machine.turnonfn = TurnOn
-	-- inst.components.machine.turnofffn = TurnOff
-	-- inst.components.machine.cooldowntime = 0.5
+	inst:AddComponent("machine")
+	inst.components.machine.turnonfn = TurnOn
+	inst.components.machine.turnofffn = TurnOff
+	inst.components.machine.cooldowntime = 0.5
 	
 	inst.mmstorage = SpawnPrefab("miningmachine_storage")
 	inst.mmstorage.Transform:SetPosition(inst:GetPosition():Get())
@@ -62,7 +218,7 @@ local function miningmachinefn()
 					-- )
 	-- inst.components.fueled.OnSave = OnSaveFueled
 	
-	-- inst:AddComponent("lootdropper")
+	inst:AddComponent("lootdropper")
 	
 	-- inst:AddComponent("workable")
 	-- inst.components.workable:SetWorkAction(ACTIONS.HAMMER)
@@ -71,8 +227,13 @@ local function miningmachinefn()
 	-- inst.components.workable:SetOnWorkCallback(onhit)
 	-- inst:ListenForEvent("onbuilt", onbuilt)
 	
+	inst:AddComponent("mnzmachines")
+	inst.leaktask = inst:DoPeriodicTask(TUNING.TOTAL_DAY_TIME/4, CheckForEscapeMobs) -- A mob can escape 4 times per day. This value should be tuned (?)
+	
 	-- inst.OnSave = onsave 
 	-- inst.OnLoad = onload
+	
+	inst.components.machine:TurnOn()
 	
 	MakeHauntableLaunch(inst)
 	
@@ -154,10 +315,43 @@ local function mywidgetsetup(container, prefab, data)
     end
 end
 
+function MMSwidgetparams.itemtestfn(container, item, slot) 
+	local owner = item.components.inventoryitem and item.components.inventoryitem.owner
+	if owner ~= nil and owner:HasTag("player") then
+		return false -- We don't want the player to be able to put stuffs in this storage do we? We just want them to be able to collect what's in.
+	else
+		return true
+	end
+end
+
 local function OnWidgetUpdate(inst)
 	containers.widgetsetup = mywidgetsetup
     inst.replica.container:WidgetSetup(inst.prefab, MMSwidgetparams)
 	containers.widgetsetup = prev_widgetsetup
+end
+
+local function OnOpenMMS(inst)
+	print("I call the OnOpenMMS")
+	print("-> Found mnzmachines component : ", inst.components.mnzmachines)
+	print("-> surpriseinchest exists : ", inst.components.mnzmachines.surpriseinchest, "and if of size ", #inst.components.mnzmachines.surpriseinchest)
+	
+	for k, v in pairs(inst.components.mnzmachines.surpriseinchest) do
+		print(k, " / ", v)
+	end
+	
+	if inst.components.mnzmachines and inst.components.mnzmachines.surpriseinchest and #inst.components.mnzmachines.surpriseinchest then
+		for k, v in pairs(inst.components.mnzmachines.surpriseinchest) do
+			for n = 1, v, 1 do
+				inst:DoTaskInTime(0.5, function()
+											local surprisemob = SpawnPrefab(k)
+											surprisemob.Transform:SetPosition(inst.Transform:GetWorldPosition())
+										end
+								)
+			end
+		end
+		
+		inst.components.mnzmachines.surpriseinchest = {}
+	end
 end
 
 local function miningmachine_storagefn()
@@ -190,6 +384,9 @@ local function miningmachine_storagefn()
     inst.components.container:WidgetSetup(inst.prefab, MMSwidgetparams)
 	containers.widgetsetup = prev_widgetsetup
 	inst._widgetupdate:set(true)
+	inst.components.container.onopenfn = OnOpenMMS
+	
+	inst:AddComponent("mnzmachines")
 	
 	inst.SetFollowTarget = SetFollowTarget
 	
